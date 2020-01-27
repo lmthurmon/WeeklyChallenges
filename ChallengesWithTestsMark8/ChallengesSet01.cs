f87d8adefeda6ab528903ce79b1933d88e168e14
Landon Thurmon <landonthurmon1@gmail.com>
2020-01-27 13:27:21 -0600

    Set 01 Solved

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -10,7 +10,7 @@ namespace ChallengesWithTestsMark8
             if (num1 == num2)
             { return true; }
             else
-            {return false; }
+            { return false; }
         }
 
         public double Subtract(double minuend, double subtrahend)
@@ -25,7 +25,7 @@ namespace ChallengesWithTestsMark8
         {
             var addition = number1 + number2;
             return addition;
-            
+
         }
 
         public int GetSmallestNumber(int number1, int number2)
@@ -34,25 +34,34 @@ namespace ChallengesWithTestsMark8
             { return number1; }
             else
             { return number2; }
-            
+
         }
 
         public long Multiply(long factor1, long factor2)
         {
             var mult = factor1 * factor2;
             return mult;
-            
+
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            return "Landon";
-           // throw new NotImplementedException(); 
+
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+
+            else 
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
